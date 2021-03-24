a882a9cb709d318f7f94db84b188d536986ed431
Dmitry993 <43587882+Dmitry993@users.noreply.github.com>
2021-03-24 14:07:39 +0300

    add model and mock repositry

---
+++ src/CommandAPI/Data/MockCommandAPIRepo.cs
@@ -0,0 +1,41 @@
+﻿using CommandAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CommandAPI.Data
+{
+    public class MockCommandAPIRepo : ICommandAPIRepo
+    {
+        public void CreateCommand(Command cmd)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteCommand(Command cmd)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Command> GetAllCommands()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Command GetCommandById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool SaveChanges()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateCommand(Command cmd)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
